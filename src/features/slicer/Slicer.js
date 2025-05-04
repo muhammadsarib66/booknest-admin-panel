8834ee248e6e70889d8035777d920afcba002043
muhammadsarib66 <saribnoor0310@gmail.com>
2025-05-05 02:36:10 +0500

    add production url

--- src/features/slicer/Slicer.js
+++ src/features/slicer/Slicer.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 // export const baseUrl = "http://192.168.53.53:3000/"; // change your Ip acording to your system
-export const baseUrl = "http://192.168.100.19:3000/"; // change your Ip acording to your system
+// export const baseUrl = "http://192.168.100.110:3000/"; // change your Ip acording to your system
+export const baseUrl = "https://book-nest-backend-production.up.railway.app/"; // change your Ip acording to your system
 
 const token = localStorage.getItem("NBAdminToken");
 
